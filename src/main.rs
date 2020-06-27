870cb4fa9d67255e20f0dfba87131e331a8170e3
ap <47916875+atomicparade@users.noreply.github.com>
2020-06-26 21:55:48 -0400

    Split commands into first_word and rest

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use dotenv::dotenv;
-use log::{error, info};
+use log::{debug, error, info};
 use regex::Regex;
 use std::sync::Arc;
 use std::{env, process};
@@ -21,7 +21,11 @@ impl TypeMapKey for ShardManagerKey {
     type Value = Arc<Mutex<ShardManager>>;
 }
 
-struct Command<'a>(&'a str);
+struct Command<'a> {
+    entire: &'a str,
+    first_word: &'a str,
+    rest: &'a str,
+}
 
 fn is_command<'a>(ctx: &Context, msg: &'a Message) -> Option<Command<'a>> {
     // Check whether the message begins with a mention of the bot
@@ -30,22 +34,32 @@ fn is_command<'a>(ctx: &Context, msg: &'a Message) -> Option<Command<'a>> {
         .get::<BotUserIdKey>()
         .expect("Unable to retrieve bot user ID");
 
-    let re_pattern = format!(r"<@!?{}>\s*((?s).*)", bot_user_id);
-    let re_command = Regex::new(&re_pattern).expect("Unable to create command matching pattern");
+    let re_pattern = format!(r"<@!?{}>\s*(\S*)\s*((?s).*)", bot_user_id);
+    let re_command =
+        Regex::new(&re_pattern).expect("Unable to create command matching pattern with bot ID");
 
     if let Some(captures) = re_command.captures(&msg.content) {
-        return Some(Command(
-            captures
-                .get(1)
-                .expect("Unable to extract command text from message containing command")
-                .as_str(),
-        ));
+        return Some(Command {
+            entire: captures.get(0).expect("Unable to extract entire command text from message beginning with bot ID").as_str(),
+            first_word: captures.get(1).expect("Unable to extract first word of command text from message beginning with bot ID").as_str(),
+            rest: captures.get(2).expect("Unable to extract rest of command text from message beginning with bot ID").as_str(),
+        });
     }
 
     // Check whether this is a DM
     if let Some(channel) = msg.channel(ctx) {
         if channel.private().is_some() {
-            return Some(Command(&msg.content));
+            let re_pattern = r"(\S*)\s*((?s).*)";
+            let re_command = Regex::new(re_pattern)
+                .expect("Unable to create command matching pattern without bot ID");
+
+            if let Some(captures) = re_command.captures(&msg.content) {
+                return Some(Command {
+                    entire: captures.get(0).expect("Unable to extract entire command text from message beginning without bot ID").as_str(),
+                    first_word: captures.get(1).expect("Unable to extract first word of command text from message beginning without bot ID").as_str(),
+                    rest: captures.get(2).expect("Unable to extract rest of command text from message beginning without bot ID").as_str(),
+                });
+            }
         }
     }
 
@@ -67,31 +81,42 @@ impl EventHandler for Handler {
 
     fn message(&self, ctx: Context, msg: Message) {
         let command = match is_command(&ctx, &msg) {
-            Some(Command(command)) => command,
+            Some(command) => command,
             None => return,
         };
 
-        if command.to_lowercase() == "quit" {
-            // TODO: Validate the user's authority
+        debug!(
+            "Received command; first word: \"{}\", rest: \"{}\"",
+            command.first_word, command.rest
+        );
 
-            info!(
-                "Quitting at the request of {}#{}",
-                msg.author.name, msg.author.discriminator
-            );
+        match command.first_word.to_lowercase().as_str() {
+            "auth" => {
+                // TODO
+            }
+            "quit" => {
+                // TODO: Validate the user's authority
 
-            let data = ctx.data.read();
+                info!(
+                    "Quitting at the request of {}#{}",
+                    msg.author.name, msg.author.discriminator
+                );
 
-            let shard_manager = match data.get::<ShardManagerKey>() {
-                Some(shard_manager) => shard_manager,
-                None => {
-                    process::exit(1);
-                }
-            };
+                let data = ctx.data.read();
 
-            shard_manager.lock().shutdown_all();
-        } else {
-            info!("Creating image for string {}", command);
-            // TODO
+                let shard_manager = match data.get::<ShardManagerKey>() {
+                    Some(shard_manager) => shard_manager,
+                    None => {
+                        process::exit(1);
+                    }
+                };
+
+                shard_manager.lock().shutdown_all();
+            }
+            _ => {
+                info!("Creating image for string {}", command.entire);
+                // TODO
+            }
         }
     }
 }
